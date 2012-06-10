a3ea60530dd7d2efb3695c91df5e739a1a3b8a0c
Your Name <tingan@clockies.com>
2012-06-10 16:47:28 +0200

    Added javascript file

---
+++ template.js
@@ -0,0 +1,84 @@
+
+
+var SC = (function($){
+
+	
+	var SC = {}, // Global variable
+	
+	
+	/* 
+	-------------------------------------------------------
+	Private variables 
+	-------------------------------------------------------
+	*/
+
+	// Current list that is showing in slide show
+	YOUR_FIRST_VARIABLE = null
+
+	
+
+	/* 
+	-------------------------------------------------------
+	Public variables 
+	-------------------------------------------------------
+	*/
+
+	// Ticket text show in slide show
+	SC.your_first_public_variable;
+
+	/**
+	 *  @object 
+	 * 	  Example object, this is an example project
+	 *  @method
+	 */
+	SC.example_object = {
+		method1: function () {
+
+		}
+	}
+
+
+	/* 
+	-------------------------------------------------------
+	Constructor
+	-------------------------------------------------------
+	*/
+
+
+	/**
+	 *	@constructor
+	 */
+	SC.constructor = function () {
+	
+	}
+
+	/* 
+	-------------------------------------------------------
+	Private methods 
+	-------------------------------------------------------
+	*/
+	function example_of_private_function () {
+
+	}
+
+	/* 
+	-------------------------------------------------------
+	Public methods 
+	-------------------------------------------------------
+	*/
+	SC.example_of_public_function () {
+
+	}
+	
+
+	// Execute constructor
+	SC.constructor();
+
+	// Return object
+	return SC;
+	
+
+})(jQuery);
+
+
+
